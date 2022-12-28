b53454714b94bed4a4c842308e4a20a474676104
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2022-12-28 23:19:22 +0100

    Create basic html(jsx) structure, clean project and install react icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FiSearch } from "react-icons/fi";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      {/* header */}
+      <header className="header">
+        <div className="header__wrapper">
+          <p className="header__clock">19:00</p>
+          <button className="header__hamburger">
+            <div className="header__hamburger-line"></div>
+            <div className="header__hamburger-line"></div>
+            <div className="header__hamburger-line"></div>
+          </button>
+        </div>
       </header>
+
+      {/* hero */}
+      <section className="hero">
+        <div className="hearo__image-box">
+          <img
+            className="hearo__picture"
+            src="https://www.kamil--m.com/education/assets/english.jpg"
+            alt="english flag"
+          />
+          <img className="hearo__logo" src={logo} alt="logo" />
+        </div>
+      </section>
+
+      {/* invitation */}
+      <section className="invitation">
+        <div className="invitation__inputs">
+          <input
+            type="radio"
+            name="invitation"
+            id="getStarted"
+            className="invitation__input"
+            checked
+          />
+          <input
+            type="radio"
+            name="invitation"
+            id="Search"
+            className="invitation__input"
+          />
+        </div>
+        <h1 className="invitation__title">Welcome to the English Helper app</h1>
+        <p className="invitation__description">
+          learn the most popular words of the English language
+        </p>
+        <button className="invitation__btn">
+          <span className="invitation__btn-text">Get started</span>
+          {/* arrow icon from react icons */}
+          <HiOutlineArrowNarrowRight />
+        </button>
+        <div className="invitation__search-box">
+          <label htmlFor="search" className="invitation__search-label">
+            Search
+          </label>
+          <input type="search" name="search" id="search" />
+          {/* search icon from react icons */}
+          <FiSearch />
+        </div>
+      </section>
     </div>
   );
 }
